96aae3669b219f20b316a5d7b5a9860f49d797e5
rmunoz18 <rogelioloaiza64@gmail.com>
2019-05-13 13:27:13 -0600

    mi parte actualizada

--- Desktop/Proyecto/src/app/components/contabilizacion/contabilizacion.component.ts
+++ Desktop/Proyecto/src/app/components/contabilizacion/contabilizacion.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit,ChangeDetectorRef,OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {PeriodicElement} from '../interfaces/periodic-element';
-import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource,MatDialog} from '@angular/material';
 
 export interface PeriodicElement {
   descripcion: string;
   codigo: number;
-}
-
+} 
 @Component({
-  selector: 'app-pasos-cierre',
-  templateUrl: './pasos-cierre.component.html',
-  styleUrls: ['./pasos-cierre.component.scss']
+  selector: 'app-contabilizacion',
+  templateUrl: './contabilizacion.component.html',
+  styleUrls: ['./contabilizacion.component.scss']
 })
-
-export class PasosCierreComponent implements OnInit {
-
+export class ContabilizacionComponent implements OnInit {
   displayedColumnss = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   numero: number;
@@ -26,44 +22,50 @@ export class PasosCierreComponent implements OnInit {
   selectedValue: string = "";
 
 
+  
   ngOnInit() {
   }
 
-  constructor(public dialog: MatDialog){
-  }
+  constructor(public dialog: MatDialog) { }
+
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  openDialog1():void{ ///AGREGAR
-    const dialogRef = this.dialog.open(agregarPasosCierre,{
-      width: '650px',
-      height: '530px',
-      data: {numero: this.numero, descripcion: this.descripcion,}
-    });
-  
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.numero = result;
-    });
-  }
 }
+
+openDialog1():void{ ///AGREGAR
+  const dialogRef = this.dialog.open(agregarContabilizacion,{
+    width: '650px',
+    height: '530px',
+    data: {numero: this.numero, descripcion: this.descripcion,}
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+    this.numero = result;
+  });
+}
+
+}
+
+
 
 @Component({ //Agregar
 
-  selector: 'app-pasos-cierre',
-  templateUrl: './agregarPasosCierre.html',
-  styleUrls:['./pasos-cierre.component.scss']
-
+  selector: 'app-contabilizacion',
+  templateUrl: './agregarContabilizacion.html',
+  styleUrls:['./contabilizacion.component.scss']
+  
 })
 
-export class agregarPasosCierre implements OnInit {
+
+
+
+export class agregarContabilizacion implements OnInit {
   checked = false;
   indeterminate = false;
   disabled = false;
 
-  
+
   constructor(){}
   displayedColumns = ['position', 'name', 'weight', 'symbol'];
   selectedValue: string = "";
@@ -73,6 +75,9 @@ export class agregarPasosCierre implements OnInit {
 
   }
 }
+
+
+
 const ELEMENT_DATA: PeriodicElement[] = [
   {codigo: 1, descripcion: 'Hydrogen'},
   {codigo: 2, descripcion: 'Helium'},
